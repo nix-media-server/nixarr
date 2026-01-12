9fb03b0c99a81fa347bd9472add6e89804ed871e
lschuetze <lschuetze@mpi-sws.org>
2026-01-12 22:59:14 +0100

    sabnzbd: maintain old and new sabnzbd module in parallel

--- nixarr/sabnzbd/default.nix
+++ nixarr/sabnzbd/default.nix
@@ -131,15 +131,9 @@ in {
     };
   };
 
-  config = let
-    concatStringsCommaIfExists = with lib.strings;
-      stringList: (
-        optionalString (builtins.length stringList > 0) (
-          concatStringsSep "," stringList
-        )
-      );
-  in
-    mkIf (nixarr.enable && cfg.enable) {
+  config = lib.mkIf (nixarr.enable && cfg.enable) (lib.mkMerge [
+    # Common config
+    {
       assertions = [
         {
           assertion = cfg.vpn.enable -> nixarr.vpn.enable;
@@ -193,24 +187,6 @@ in {
         package = cfg.package;
         user = globals.sabnzbd.user;
         group = globals.sabnzbd.group;
-
-        settings = {
-          misc = {
-            host =
-              if cfg.openFirewall
-              then "0.0.0.0"
-              else if cfg.vpn.enable
-              then "192.168.15.1"
-              else "127.0.0.1";
-            port = cfg.guiPort;
-            download_dir = "${cfg.incompleteDir}/usenet/.incomplete";
-            complete_dir = "${nixarr.mediaDir}/usenet/manual";
-            dirscan_dir = "${nixarr.mediaDir}/usenet/watch";
-            host_whitelist = concatStringsCommaIfExists cfg.whitelistHostnames;
-            local_ranges = concatStringsCommaIfExists cfg.whitelistRanges;
-            permissions = "775";
-          };
-        };
       };
 
       networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [cfg.guiPort];
@@ -252,5 +228,136 @@ in {
           };
         };
       };
-    };
+    }
+
+    # Module for nixpkgs version < 26.05
+    # We check for the pre-git version suffix because that is the version returned by a
+    # flakes-based installation of nixpkgs.
+    # 26.05pre-git < 26.05, so this is the first version with the new module
+    (lib.optionalAttrs (lib.versionOlder lib.version "26.05pre-git") (let
+      ini-file-target = "${cfg.stateDir}/sabnzbd.ini";
+      concatStringsCommaIfExists = with lib.strings;
+        stringList: (
+          optionalString (builtins.length stringList > 0) (
+            concatStringsSep "," stringList
+          )
+        );
+
+      user-configs = {
+        misc = {
+          host =
+            if cfg.openFirewall
+            then "0.0.0.0"
+            else if cfg.vpn.enable
+            then "192.168.15.1"
+            else "127.0.0.1";
+          port = cfg.guiPort;
+          download_dir = "${nixarr.mediaDir}/usenet/.incomplete";
+          complete_dir = "${nixarr.mediaDir}/usenet/manual";
+          dirscan_dir = "${nixarr.mediaDir}/usenet/watch";
+          host_whitelist = concatStringsCommaIfExists cfg.whitelistHostnames;
+          local_ranges = concatStringsCommaIfExists cfg.whitelistRanges;
+          permissions = "775";
+        };
+      };
+
+      ini-base-config-file = pkgs.writeTextFile {
+        name = "base-config.ini";
+        text = lib.generators.toINI {} user-configs;
+      };
+
+      fix-config-permissions-script = pkgs.writeShellApplication {
+        name = "sabnzbd-fix-config-permissions";
+        runtimeInputs = with pkgs; [util-linux];
+        text = ''
+          if [ ! -f ${ini-file-target} ]; then
+            echo 'FAILURE: cannot change permissions of ${ini-file-target}, file does not exist'
+            exit 1
+          fi
+
+          chmod 600 ${ini-file-target}
+          chown ${globals.sabnzbd.user}:${globals.sabnzbd.group} ${ini-file-target}
+        '';
+      };
+
+      user-configs-to-python-list = with lib;
+        attrsets.collect (f: !builtins.isAttrs f) (
+          attrsets.mapAttrsRecursive (
+            path: value:
+              "sab_config_map['"
+              + (lib.strings.concatStringsSep "']['" path)
+              + "'] = '"
+              + (builtins.toString value)
+              + "'"
+          )
+          user-configs
+        );
+
+      apply-user-configs-script =
+        pkgs.writers.writePython3Bin "sabnzbd-set-user-values" {
+          libraries = [pkgs.python3Packages.configobj];
+        } ''
+          # flake8: noqa
+          from pathlib import Path
+          from configobj import ConfigObj
+
+          sab_config_path = Path("${ini-file-target}")
+          if not sab_config_path.is_file() or sab_config_path.suffix != ".ini":
+              raise Exception(f"{sab_config_path} is not a valid config file path.")
+
+          sab_config_map = ConfigObj(str(sab_config_path))
+
+          ${lib.strings.concatStringsSep "\n" user-configs-to-python-list}
+
+          sab_config_map.write()
+        '';
+    in {
+      systemd.tmpfiles.rules = [
+        "C ${cfg.stateDir}/sabnzbd.ini - - - - ${ini-base-config-file}"
+      ];
+
+      services.sabnzbd = {
+        configFile = "${cfg.stateDir}/sabnzbd.ini";
+      };
+
+      systemd.services.sabnzbd.serviceConfig = {
+        ExecStartPre = lib.mkBefore [
+          ("+" + fix-config-permissions-script + "/bin/sabnzbd-fix-config-permissions")
+          (apply-user-configs-script + "/bin/sabnzbd-set-user-values")
+        ];
+        Restart = "on-failure";
+        StartLimitBurst = 5;
+      };
+    }))
+    # Module for nixpkgs version 26.05 or later
+    # See above why we check for 26.05pre-git instead of 26.05
+    (lib.optionalAttrs (lib.versionAtLeast lib.version "26.05pre-git") (let
+      concatStringsCommaIfExists = with lib.strings;
+        stringList: (
+          optionalString (builtins.length stringList > 0) (
+            concatStringsSep "," stringList
+          )
+        );
+    in {
+      services.sabnzbd = {
+        settings = {
+          misc = {
+            host =
+              if cfg.openFirewall
+              then "0.0.0.0"
+              else if cfg.vpn.enable
+              then "192.168.15.1"
+              else "127.0.0.1";
+            port = cfg.guiPort;
+            download_dir = "${cfg.incompleteDir}/usenet/.incomplete";
+            complete_dir = "${nixarr.mediaDir}/usenet/manual";
+            dirscan_dir = "${nixarr.mediaDir}/usenet/watch";
+            host_whitelist = concatStringsCommaIfExists cfg.whitelistHostnames;
+            local_ranges = concatStringsCommaIfExists cfg.whitelistRanges;
+            permissions = "775";
+          };
+        };
+      };
+    }))
+  ]);
 }
